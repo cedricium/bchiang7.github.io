529b5169ec42c30622a4f0e7b19aa882f4b5053f
Cedricium <amaya.cedric@gmail.com>
2020-09-15 13:59:54 -0700

    Create custom 404 page

---
+++ pages/404.js
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+
+import Layout from '@/components/layout'
+
+export default function Custom404() {
+  return (
+    <Layout>
+      <section className='not-found'>
+        <img src='/img/emojis/thinking-face.png' alt='thinking face emoji' />
+        <h1>Hmmm…</h1>
+        <h2>
+          Looks like you may have lost your way. Why don't you head back{` `}
+          <Link href='/'>home</Link>.
+        </h2>
+      </section>
+    </Layout>
+  )
+}
