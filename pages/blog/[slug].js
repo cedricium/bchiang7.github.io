b874ac7f0c4ea22a9f0513a086a183d908bcc4bb
Cedricium <amaya.cedric@gmail.com>
2020-09-22 16:45:32 -0700

    Update layout and styling of blog posts

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -2,21 +2,23 @@ import Link from 'next/link'
 
 import Layout from 'components/layout'
 import Date from 'components/date'
+import Blurb from 'components/blurb'
 
 import { getAllPostSlugs, getPostData } from 'lib/posts'
 
 export default function Post({ postData }) {
   return (
     <Layout title={postData.title}>
-      <section className='blog-post'>
-        <h3 className='blog-post__backlink'>
-          <Link href='/blog'>
+      <section className="blog-post">
+        <h3 className="blog-post__backlink">
+          <Link href="/blog">
             <a>&larr; Back to Blog</a>
           </Link>
         </h3>
-        <h1 className='blog-post__title'>{postData.title}</h1>
+        <h1 className="blog-post__title">{postData.title}</h1>
         <Date dateString={postData.date} />
         <article dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        <Blurb />
       </section>
     </Layout>
   )
